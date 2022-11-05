53b415d92bb2c0ee3ff1fc7a803037705b35935c
Hwang-Junsu <hjs77728@gmail.com>
2022-11-05 13:17:16 +0900

    carrot-market

--- pages/streams/index.tsx
+++ pages/streams/index.tsx
@@ -14,7 +14,7 @@ const Streams: NextPage = () => {
     const {data} = useSWR<StreamResponse>(`/api/streams`);
     return (
         <Layout hasTabBar title="라이브">
-            <div className=" divide-y-[1px] space-y-4">
+            <div className="divide-y-[1px] space-y-4">
                 {data?.streams.map((stream) => (
                     <Link key={stream.id} href={`/streams/${stream.id}`}>
                         <a className="pt-4 block  px-4">
